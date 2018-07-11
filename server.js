8a579f84dac408c4bcb4b55b7f5f2f8ba1b37441
David Daugherty <david.m.daugherty@gmail.com>
2018-07-11 07:07:57 -0600

    Set up express and knex

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
-// const express = require('express');
-// const app = express();
-// const bodyParser = require('body-parser');
-// const environment = process.env.NODE_ENV || 'development';
-// const configuration = require('./knexfile')[environment];
-// const database = require('knex')(configuration);
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
+app.set('port', process.env.PORT || 3000);
+app.locals.title = 'Chat Box';
+
+app.get('/', (request, response) => {
+  response.send('Oh hey Chat Box');
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on ${app.get('port')}.`);
+});
